894beba9609ee6c53a4141a1f9e532497c7360bf
dingdingLee <vickytory1015@gmail.com>
2021-02-10 16:36:38 +0900

    setting

---
+++ index.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
+
+const app = express();
+
+app.get('/', (req, res) => 
+    res.sendFile(__dirname +'/app/index.html')
+);
+
+
+
+const port = 3000;
+app.listen(port, () => console.log('Server start on ${port}'));
